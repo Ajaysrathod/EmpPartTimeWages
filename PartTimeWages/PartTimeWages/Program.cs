0d0473d6aef095d54b2686a371883fdbb1264df2
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-01-06 21:10:46 +0530

    add Emp Part Time Wages program complete

---
+++ PartTimeWages/PartTimeWages/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PartTimeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ISPARTTIME = 1;
+            int ISFULLTIME = 2;
+            int EMPRATEPERHOUR = 20;
+            int emphrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == ISPARTTIME)
+            {
+                emphrs = 4;
+
+            }
+            else if (empCheck == ISFULLTIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empWage = emphrs * EMPRATEPERHOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
